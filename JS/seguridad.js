500790b636b307c421c577ca057ecf3b1e0ecd6a
Claudio Fuentes <ccfuentes91@gmail.com>
2023-09-14 23:28:55 -0300

    en proceso de modificacion de datos de cuenta

--- JS/seguridad.js
+++ JS/seguridad.js
@@ -6,49 +6,80 @@ const eliminarCuenta = document.querySelector('#eliminarCuenta')
 
 
 class EditDatos {
-    constructor(newName, newLastName, newMail, newGender, newJob){
+    constructor(newName, newLastName, newMail, newGender, newJob) {
         this.newName = newName;
         this.newLastName = newLastName;
-        this.newMail =newMail;
+        this.newMail = newMail;
         this.newGender = newGender;
         this.newJob = newJob;
-    }  
+    }
 }
 
-modificarDatos.onclick = () =>{
-    datosUsuario.innerHTML = ""
+modificarDatos.onclick = () => {
+    // crea inputs para modificar datos
+
+
+    datosUsuario.innerHTML = `<form id="modDatos">`
+    const newForm = document.querySelector('#modDatos')
+
     const nombre = document.createElement('input')
     nombre.setAttribute('type', 'text')
     nombre.setAttribute('id', 'newName')
-    nombre.setAttribute('placeholder', `${usuarioActivo.nombre}`)
-    datosUsuario.appendChild(nombre)
+    nombre.value = `${usuarioActivo.nombre}`
+    newForm.appendChild(nombre)
 
     const lastname = document.createElement('input')
     lastname.setAttribute('type', 'text')
     lastname.setAttribute('id', 'newLastName')
-    lastname.setAttribute('placeholder', `${usuarioActivo.apellido}`)
-    datosUsuario.appendChild(lastname)
+    lastname.value = `${usuarioActivo.apellido}`
+    newForm.appendChild(lastname)
 
     const mail = document.createElement('input')
-    mail.setAttribute('type', 'text')
+    mail.setAttribute('type', 'email')
     mail.setAttribute('id', 'newMail')
-    mail.setAttribute('placeholder', `${usuarioActivo.correo}`)
-    mail.innerHTML = '<br>'
-    datosUsuario.appendChild(mail)
+    mail.value = `${usuarioActivo.correo}`
+    newForm.appendChild(mail)
 
-    const newgender = document.createElement('input')
-    newgender.setAttribute('type', 'text')
-    newgender.setAttribute('id', 'newGender')
-    newgender.setAttribute('placeholder', `${usuarioActivo?.genero || "Genero"}`)
-    newgender.innerHTML = '<br>'
-    datosUsuario.appendChild(newgender)
+    const genero = document.createElement('input')
+    genero.setAttribute('type', 'text')
+    genero.setAttribute('id', 'newGender')
+    genero.value = `${usuarioActivo?.genero || "Genero"}`
+    newForm.appendChild(genero)
 
-    const newjob = document.createElement('input')
-    newjob.setAttribute('type', 'text')
-    newjob.setAttribute('id', 'newJob')
-    newjob.setAttribute('placeholder', `${usuarioActivo?.trabajo || "Profesion"}`)
-    newjob.innerHTML = '<br>'
-    datosUsuario.appendChild(newjob)
+    const profes = document.createElement('input')
+    profes.setAttribute('type', 'text')
+    profes.setAttribute('id', 'newJob')
+    profes.value = `${usuarioActivo?.trabajo || "Profesion"}`
+    newForm.appendChild(profes)
+
+    const botonGuardar = document.createElement('button')
+    botonGuardar.setAttribute('id', 'btnGuardar')
+    botonGuardar.innerText = 'Guardar'
+    newForm.appendChild(botonGuardar)
+    const guardar = document.querySelector('#btnGuardar')
+
+
+
+
+
+    guardar.onclick = () => {
+        const newName = document.querySelector('#newName')
+        const newLastName = document.querySelector('#newLastName')
+        const newMail = document.querySelector('#newMail')
+        const newGender = document.getElementById('newGender')
+        const newJob = document.getElementById('newJob')
+        
+
+        const nuevosDatos = new EditDatos(newName, newLastName, newMail, newGender, newJob)
+        const pos = usuarios.indexOf(usuarioActivo)
+        usuarios.splice(pos, 1, nuevosDatos)
+        localStorage.setItem('usuarios', JSON.stringify(usuarios))
+        sessionStorage.setItem('usuarioActivo', JSON.stringify(nuevosDatos))
+    }
+
+
+
+
 
 
 }
